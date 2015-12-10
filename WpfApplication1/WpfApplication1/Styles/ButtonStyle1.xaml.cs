7faa26f9b605e99533da04c775dc1fa7b5b6ebb5
phoenixdd28 <phxphxphxphxphx@gmail.com>
2015-12-10 19:03:27 +0300

    V1.1

---
+++ WpfApplication1/WpfApplication1/Styles/ButtonStyle1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1.Styles
+{
+    /// <summary>
+    /// Логика взаимодействия для ButtonStyle1.xaml
+    /// </summary>
+    public partial class ButtonStyle1 : Window
+    {
+        public ButtonStyle1()
+        {
+            InitializeComponent();
+        }
+    }
+}
